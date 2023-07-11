f52a8e176ce209f8bb6b968d5f607a75f9cb6517
Jiwonii97 <wldnjs3633@naver.com>
2023-07-11 18:09:18 +0900

    feat) 페이지 개행 작업 진행

--- streamlit/text_analysis.py
+++ streamlit/text_analysis.py
@@ -1,7 +1,10 @@
 import json
 import requests
 import streamlit as st
+
+# streamlit tools
 from streamlit_tags import st_tags
+from streamlit_space import space
 
 
 # 문서 분석 방식
@@ -12,57 +15,8 @@ def text_analysis(title):
     st.title(title)
     st.write("---")
 
-    with st.expander("# Custom"):
+    space(lines=2)
 
-        # multiselect
-        st.write('### 장르 (Genre)')
-        options_0 = st.multiselect(
-            label='🎼 배경음악의 장르를 정해주세요.',
-            options=['Green', 'Yellow', 'Red', 'Blue'],
-            default=[])
-
-        st.write('### 악기 (Musical Instruments)')
-        options_1 = st.multiselect(
-            label='🥁 배경음악의 악기를 정해주세요.',
-            options=['Green', 'Yellow', 'Red', 'Blue'],
-            default=[])
-
-        st.write('### 분위기 (Mood)')
-        options_2 = st.multiselect(
-            label='📣 배경음악의 분위기를 정해주세요.',
-            options=['Green', 'Yellow', 'Red', 'Blue'],
-            default=[])
-
-        # 사용자 keywords 생성
-        options_3 = st_tags(
-            label='### 그 외 (ETC)',
-            text='그 외에 추가하고 싶은 곡 정보를 입력해주세요.',
-            value=['Blue'],
-            suggestions=['Green', 'Yellow', 'Red', 'Blue'],
-            key="etc_choice")
-    
-        options_4 = st.selectbox(
-            label='생성할 음악의 시간을 정해주세요',
-            options=['0:10', '0:30', '1:00', '1:30', '2:00', '3:00'],
-            )
-
-        options_5 = st.radio('음악의 빠르기를 선택해 주세요', ['slow', 'normal', 'fast'])
-    
+    space(lines=2)
     # text area
-    text = st.text_area('분석을 진행하고 싶은 텍스트를 입력하세요 👉')
-    
-    if st.button("SUBMIT"):
-        # TO DO : 리스트를 모델 서버로 전달 -> 다시 생성된 음악 파일 받고 올림
-        min, sec = map(int,options_4.split(':'))
-        options_4 = min*60 + sec
-        inputs = {
-            "genre": options_0,
-            "instrument": options_1,
-            "mood": options_2,
-            "etc": options_3,
-            "text": text,
-            "time": options_4,
-            "tempo": options_5,
-        }
-        st.write(inputs)
-        requests.post(url = "http://127.0.0.1:8000/text_analysis", data = json.dumps(inputs))
+    space(lines=1)
