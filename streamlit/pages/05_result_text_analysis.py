7166f136acafcc59bd32870c2b2e8619864f267a
Jiwonii97 <wldnjs3633@naver.com>
2023-07-11 22:28:17 +0900

    feat) 생성된 음악 정보를 바탕으로 객체 생성후 출력하는 코드 작성

--- streamlit/pages/05_result_text_analysis.py
+++ streamlit/pages/05_result_text_analysis.py
@@ -1,7 +1,21 @@
+from PIL import Image
+import os
+
+import streamlit as st
+from streamlit_space import space
+from pathlib import Path
+
+downloads_path = os.path.join(Path.home(), "Downloads")
+
+# 문서 분석 방식
+TITLE = "문서 분석 방식"
+IMAGE_ICON_PATH = "./streamlit/assets/music_icon.png"
 MUSIC_PATH = "./streamlit/assets/test_music.wav"
 
 TEST_CAPTION = ["Orchestral", "With a strings", "Cinematic", "Slow bpm"]
 button_num = 0
+
+
 class TextAnalysisContent():
     def __init__(self, caption, file):
         self.caption = caption
@@ -42,5 +56,27 @@ class TextAnalysisContent():
             )
             button_num += 1     # 버튼은 key값을 지정해야 하기때문에 임의로 Key를 지정
         space(lines=2)      # 컨텐츠 구분을 짓기 위한 개행 처리
+
+
+def result_text_analysis():
     summary_text = "Orchestral, with a strings, cinematic, slow bpm"
     audio_file = open(MUSIC_PATH, 'rb').read()
+
+    st.title(TITLE)
+    st.write("---")
+
+    # summary text area
+    st.text_area(label="문서 요약 결과", value=summary_text,
+                 height=50, disabled=True)
+    space(lines=2)
+
+    # print contents
+    music_contents = []
+    for _ in range(5):
+        music_contents.append(TextAnalysisContent(TEST_CAPTION, audio_file))
+
+    for content in music_contents:
+        content.set_content()
+
+
+result_text_analysis()
