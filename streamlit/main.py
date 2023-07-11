dcd25718b710118c2b6b3fcd19a0532f3d5fc65f
Jiwonii97 <wldnjs3633@naver.com>
2023-07-11 22:43:24 +0900

    fix) 코드명 수정

--- streamlit/main.py
+++ streamlit/main.py
@@ -4,7 +4,6 @@ import os
 
 # streamlit tools
 import streamlit as st
-from streamlit_tags import st_tags
 
 
 BASE_PATH = "streamlit"  # 내 로컬에서 작업할 경우 오류가 나서 일단 없앰
@@ -13,5 +12,4 @@ APP_WORK = ["가이드", "카테고리 선택 방식", "문서 분석 방식"]
 
 
 st.sidebar.image(SIDEBAR_IMAGE_PATH)
-page = st.sidebar.radio("원하는 서비스를 선택하세요", APP_WORK)
-
+# page = st.sidebar.radio("원하는 서비스를 선택하세요", APP_WORK)
