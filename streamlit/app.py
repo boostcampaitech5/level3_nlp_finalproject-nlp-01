ea8deb4b6084a6b6cef861e171aba1b53987deae
김효연_T5072 <39620398+Broco98@users.noreply.github.com>
2023-07-11 14:31:51 +0900

    feat) 가이드 페이지 추가 - 설명 추가전

--- streamlit/app.py
+++ streamlit/app.py
@@ -7,17 +7,16 @@ import streamlit as st
 from streamlit_tags import st_tags
 from choice_category import choice_category
 from text_analysis import text_analysis
+from guide import guide
+
 
 BASE_PATH = "" # 내 로컬에서 작업할 경우 오류가 나서 일단 없앰
 SIDEBAR_IMAGE_PATH = os.path.join(BASE_PATH, "assets/crying_frog.png")
-APP_WORK = ["카테고리 선택 방식", "문서 분석 방식", "test"]
+APP_WORK = ["가이드", "카테고리 선택 방식", "문서 분석 방식"]
+
 
 st.sidebar.image(SIDEBAR_IMAGE_PATH)
 page = st.sidebar.radio("원하는 서비스를 선택하세요", APP_WORK)
-
-def test(title):
-    st.title(title)
-    st.write("---")
 
 
 def main():
@@ -25,11 +24,12 @@ def main():
 
     # page 선택에 따라 함수 호출
     if page == APP_WORK[0]:
-        choice_category(APP_WORK[0])
+        guide(APP_WORK[0])
     elif page == APP_WORK[1]:
-        text_analysis(APP_WORK[1])
+        choice_category(APP_WORK[1])
     elif page == APP_WORK[2]:
-        test(APP_WORK[2])
+        text_analysis(APP_WORK[2])
+    
 
 
 main()      # 메인 실행문
---
+++ streamlit/guide.py
@@ -0,0 +1,19 @@
+import json
+import requests
+import streamlit as st
+from streamlit_tags import st_tags
+
+
+# 문서 분석 방식
+
+
+def guide(title):
+
+    st.title(title)
+    st.write("---")
+
+    st.write('설명 추가할 예정')
+
+
+
+    
