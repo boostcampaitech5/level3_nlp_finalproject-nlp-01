2ba8d5f258bd1d7c8ebecc1b0c895acdbe896187
김효연_T5072 <39620398+Broco98@users.noreply.github.com>
2023-07-11 20:16:40 +0900

    feat) pages 도입

--- streamlit/pages/01_guide.py
+++ streamlit/pages/01_guide.py
@@ -7,13 +7,13 @@ from streamlit_tags import st_tags
 # 문서 분석 방식
 
 
-def guide(title):
+def guide():
 
-    st.title(title)
+    st.title("가이드")
     st.write("---")
 
     st.write('설명 추가할 예정')
 
 
-
+guide()
     
--- streamlit/pages/03_text_analysis.py
+++ streamlit/pages/03_text_analysis.py
@@ -6,10 +6,27 @@ from streamlit_tags import st_tags
 
 # 문서 분석 방식
 
+def create_exam_audio():
+    sample_rate = 44100  # 44100 samples per second
+    seconds = 2  # Note duration of 2 seconds
 
-def text_analysis(title):
+    frequency_la = 440  # Our played note will be 440 Hz
 
-    st.title(title)
+    # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+    t = np.linspace(0, seconds, seconds * sample_rate, False)
+
+    # Generate a 440 Hz sine wave
+    note_la = np.sin(frequency_la * t * 2 * np.pi)
+    return note_la
+
+def create_exam_binary():
+    binary_contents = b'example content'
+    return binary_contents
+
+
+def text_analysis():
+
+    st.title("문서 분석 방식")
     st.write("---")
 
     with st.expander("# Custom"):
@@ -66,3 +83,37 @@ def text_analysis(title):
         }
         st.write(inputs)
         requests.post(url = "http://127.0.0.1:8000/text_analysis", data = json.dumps(inputs))
+
+def result_choice_category(title, inputs):
+
+    st.title(title)
+    st.write("---")
+
+    st.write("### Caption")
+    captions = st.multiselect(
+        label='선택된 Caption',
+        options=inputs['captions'],
+        default=inputs['captions'],
+        disabled=True
+        )
+
+    col_1, col_2 = st.columns([4,1])
+
+    for i, w in enumerate(inputs['wav']):
+        col_1.audio(data=w)
+        col_2.download_button(label=f"Download{i+1}", data=w)
+
+    if st.button("Return"):
+        # TO DO : 리스트를 모델 서버로 전달 -> 다시 생성된 음악 파일 받고 올림
+        requests.get(url = "http://127.0.0.1:8000/return/choice_category")
+
+
+# 임시 input
+inputs = {
+    'captions':['1', '2', '3'],
+    'wav': [create_exam_binary(), create_exam_binary(), create_exam_binary(), create_exam_binary()]
+    }
+result_choice_category('Result', inputs)
+
+
+text_analysis()
