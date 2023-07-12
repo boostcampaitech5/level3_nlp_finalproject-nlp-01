b624e6e2c72a4da53a016a59ec82e5252f11a3dc
김효연_T5072 <39620398+Broco98@users.noreply.github.com>
2023-07-12 14:40:03 +0900

    fix) 파일 이름변경, 제출버튼 오른쪽으로 이동, text내용 수정

--- streamlit/pages/01_choice_category.py
+++ streamlit/pages/01_choice_category.py
@@ -3,74 +3,86 @@ import requests
 import numpy as np
 import streamlit as st
 from streamlit_tags import st_tags
-
-# custom
 from utils import add_logo
+from streamlit_space import space
 from constraints import PATH
 
-# 카테고리 선택 방식 Page
 
+# 카테고리 선택 페이지
+def choice_category(title, options):
 
-def choice_category():
-    add_logo(PATH.SIDEBAR_IMAGE_PATH, height=250)
-
-    st.title("카테고리 선택 방식")
+    st.title(title)
     st.write("---")
 
     # multiselect
-    st.write('### 장르 (Genre)')
-    options_0 = st.multiselect(
-        label='🎼 배경음악의 장르를 정해주세요.',
+    st.subheader('🎼 장르 (Genre)')
+    genre = st.multiselect(
+        label='생성할 음악의 장르를 선택해 주세요.',
         options=['Green', 'Yellow', 'Red', 'Blue'],
         default=['Red'])
-
-    st.write('### 악기 (Musical Instruments)')
-    options_1 = st.multiselect(
-        label='🥁 배경음악의 악기를 정해주세요.',
+    space(lines=1)
+    
+    st.subheader('🥁 악기 (Musical Instruments)')
+    Instruments = st.multiselect(
+        label='생성할 음악의 악기를 선택해 주세요.',
         options=['Green', 'Yellow', 'Red', 'Blue'],
         default=['Yellow'])
+    space(lines=1)
 
-    st.write('### 분위기 (Mood)')
-    options_2 = st.multiselect(
-        label='📣 배경음악의 분위기를 정해주세요.',
+    st.subheader('📣 분위기 (Mood)')
+    mood = st.multiselect(
+        label='생성할 음악의 분위기를 선택해 주세요.',
         options=['Green', 'Yellow', 'Red', 'Blue'],
         default=['Green'])
+    space(lines=1)
 
     # 사용자 keywords 생성
-    options_3 = st_tags(
-        label='### 그 외 (ETC)',
-        text='그 외에 추가하고 싶은 곡 정보를 입력해주세요.',
-        value=['Blue'],
+    etc = st_tags(
+        label='### 기타 (ETC)',
+        text='생성할 음악의 추가정보를 입력해 주세요',
+        value=[],
         suggestions=['Green', 'Yellow', 'Red', 'Blue'],
         key="etc_choice")
+    space(lines=1)
 
-    options_4 = st.selectbox(
-        label='생성할 음악의 시간을 정해주세요',
+    col_1, col_2 = st.columns([1,1])
+
+    col_1.subheader('⌛ 길이(Duration)')
+    duration = col_1.selectbox(
+        label='생성할 음악의 길이를 선택해 주세요',
         options=['0:10', '0:30', '1:00', '1:30', '2:00', '3:00'],
+        index=1,
     )
 
-    options_5 = st.radio('음악의 빠르기를 선택해 주세요', ['slow', 'normal', 'fast'])
+    col_2.subheader('속도 (Tempo)')
+    tempo = col_2.radio('생성할 음악의 빠르기를 선택해 주세요', ['Slow', 'Medium', 'Fast'])
 
-    if st.button("SUBMIT"):
-        # TO DO : 리스트를 모델 서버로 전달 -> 다시 생성된 음악 파일 받고 올림
-        min, sec = map(int, options_4.split(':'))
-        options_4 = min*60 + sec
+    _, button_cols = st.columns([14, 2])
+    if button_cols.button("Submit"):
+        
+        # duration 파싱
+        min, sec = map(int, duration.split(':'))
+        duration = min*60 + sec
+
+        # input 생성
         inputs = {
-            "genre": options_0,
-            "instrument": options_1,
-            "mood": options_2,
-            "etc": options_3,
-            "time": options_4,
-            "tempo": options_5,
+            "genre": genre,
+            "instruments": Instruments,
+            "mood": mood,
+            "etc": etc,
+            "duration": duration,
+            "tempo": tempo,
         }
-        st.write(inputs)
+
+        # TO DO : 리스트를 모델 서버로 전달 -> 다시 생성된 음악 파일 받고 올림
         # res = requests.post(url = "http://127.0.0.1:8000/choice_category", data = json.dumps(inputs))
+        
+        # session_state 변경 -> result 페이지로 이동
         st.session_state['state'] = 'result'
         st.experimental_rerun()
 
 
-# 결과 페이지
-
+# 임시 examp생성
 def create_exam_audio():
     sample_rate = 44100  # 44100 samples per second
     seconds = 2  # Note duration of 2 seconds
@@ -84,15 +96,12 @@ def create_exam_audio():
     note_la = np.sin(frequency_la * t * 2 * np.pi)
     return note_la
 
-
 def create_exam_binary():
     binary_contents = b'example content'
     return binary_contents
 
 
 # 결과 페이지
-
-
 def result_choice_category(title, inputs):
 
     st.title(title)
@@ -112,7 +121,9 @@ def result_choice_category(title, inputs):
         col_1.audio(data=w)
         col_2.download_button(label=f"Download{i+1}", data=w)
 
-    if st.button("Return"):
+    _, button_cols = st.columns([14, 2])
+
+    if button_cols.button("Return"):
         # TO DO : 리스트를 모델 서버로 전달 -> 다시 생성된 음악 파일 받고 올림
         st.session_state['state'] = 'execute'
         st.experimental_rerun()
@@ -126,8 +137,10 @@ if __name__ == "__main__":
     if 'state' not in st.session_state:
         st.session_state['state'] = 'execute'
 
+    add_logo(PATH.SIDEBAR_IMAGE_PATH, height=250)
+
     if st.session_state['state'] == 'execute':
-        choice_category()
+        choice_category('카테고리 선택', None)
 
     else:
         # 임시 input 생성
@@ -135,7 +148,6 @@ if __name__ == "__main__":
             'captions': ['1', '2', '3'],
             'wav': [create_exam_binary(), create_exam_binary(), create_exam_binary(), create_exam_binary()]
         }
-
         result_choice_category('Result', inputs)
 
-    del st.session_state['state']
+    del st.session_state['state'] # 페이지에서 이동할 경우 state초기화
