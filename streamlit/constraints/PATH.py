e46d6eb8c6f069bf2787e12e02cb055f7d213300
Jiwonii97 <wldnjs3633@naver.com>
2023-07-12 14:44:34 +0900

    feat) 데이터, 아이콘 경로 상수화

--- streamlit/constraints/PATH.py
+++ streamlit/constraints/PATH.py
@@ -1,4 +1,6 @@
 import os
 
-BASE_PATH = "streamlit" if os.getcwd().split('\\')[-1] != "stramlit" else ""
+BASE_PATH = "streamlit" if os.getcwd().split('\\')[-1] != "streamlit" else ""
 SIDEBAR_IMAGE_PATH = os.path.join(BASE_PATH, "assets", "sidebar_logo.png")
+DATA_PATH = os.path.join(BASE_PATH, "assets", "component_data.json")
+IMAGE_ICON_PATH = os.path.join(BASE_PATH, "assets", "music_icon.png")
