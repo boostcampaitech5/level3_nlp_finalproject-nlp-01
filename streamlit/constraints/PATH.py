d2e51e3e0a4cac6783b8ce2d463cabb6243e1f1c
Jiwonii97 <wldnjs3633@naver.com>
2023-07-13 00:17:23 +0900

    fix) DB 경로 수정

--- streamlit/constraints/PATH.py
+++ streamlit/constraints/PATH.py
@@ -2,5 +2,5 @@ import os
 
 BASE_PATH = "streamlit" if os.getcwd().split('\\')[-1] != "streamlit" else ""
 SIDEBAR_IMAGE_PATH = os.path.join(BASE_PATH, "assets", "sidebar_logo.png")
-DATA_PATH = os.path.join(BASE_PATH, "assets", "component_data.json")
+DATA_PATH = os.path.join(BASE_PATH, "assets", "category_ver1.0.0.json")
 IMAGE_ICON_PATH = os.path.join(BASE_PATH, "assets", "music_icon.png")
