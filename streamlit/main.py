09b4dbfd94f166645f8fa9e06f0e637b219dc5d5
Jiwonii97 <wldnjs3633@naver.com>
2023-07-12 11:34:44 +0900

    feat) 메인 페이지 작성 및 페이지 INFO 내용 추가

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 
-import os
-
 # streamlit tools
 import streamlit as st
 
+# custom
+from utils import add_logo
+from constraints import PATH, INFO
 
-BASE_PATH = "streamlit"  # 내 로컬에서 작업할 경우 오류가 나서 일단 없앰
-SIDEBAR_IMAGE_PATH = os.path.join(BASE_PATH, "assets/sidebar_logo.png")
-APP_WORK = ["가이드", "카테고리 선택 방식", "문서 분석 방식"]
+
+st.set_page_config(
+    page_title=INFO.PROJECT_NAME,
+    page_icon=INFO.PROJECT_ICON,
+)
 
 
-st.sidebar.image(SIDEBAR_IMAGE_PATH)
+def guide():
+    add_logo(PATH.SIDEBAR_IMAGE_PATH, height=250)
+
+    st.title("가이드")
+    st.write("---")
+
+    st.write('설명 추가할 예정')
+
+
+guide()
