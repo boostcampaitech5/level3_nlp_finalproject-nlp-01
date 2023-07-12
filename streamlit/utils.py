1d380081c301892d7ea38e20813ee02b0ba4bf96
Jiwonii97 <wldnjs3633@naver.com>
2023-07-13 00:03:14 +0900

    style) typing 추가

--- streamlit/utils.py
+++ streamlit/utils.py
@@ -47,7 +47,7 @@ def add_logo(logo_url: str, height: int = 120):
     )
 
 
-def delete_another_session_state(current_state: str):
+def delete_another_session_state(current_state: str) -> None:
     """session_state 삭제.
 
     현제 페이지의 session_state의 state만 남기고, 다른 페이지의 state를 삭제합니다.
@@ -58,4 +58,3 @@ def delete_another_session_state(current_state: str):
     for state in st.session_state.to_dict():
         if 'state' in state and state != current_state:
             del st.session_state[state]
-
