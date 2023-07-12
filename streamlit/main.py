0bb01fb23ea695b7eb6e35ba2c316ce9af137dae
Jiwonii97 <wldnjs3633@naver.com>
2023-07-12 11:35:30 +0900

    fix) 스테이징 오류 해결 과정

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,26 +1,10 @@
-# -*- coding: utf-8 -*-
-
-# streamlit tools
-import streamlit as st
-
-# custom
+    add_logo(PATH.SIDEBAR_IMAGE_PATH, height=250)
 from utils import add_logo
 from constraints import PATH, INFO
 
 
 st.set_page_config(
     page_title=INFO.PROJECT_NAME,
+# custom
     page_icon=INFO.PROJECT_ICON,
 )
-
-
-def guide():
-    add_logo(PATH.SIDEBAR_IMAGE_PATH, height=250)
-
-    st.title("가이드")
-    st.write("---")
-
-    st.write('설명 추가할 예정')
-
-
-guide()
