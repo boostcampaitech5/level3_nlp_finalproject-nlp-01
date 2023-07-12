cefbcd40909d2db90ed0b9268b0764e1d59bb99e
Jiwonii97 <wldnjs3633@naver.com>
2023-07-12 11:30:48 +0900

    feat) sidebar 로고 입히는 함수 추가

--- streamlit/utils.py
+++ streamlit/utils.py
@@ -1,5 +1,10 @@
 import json
 from typing import List
+import base64
+from pathlib import Path
+
+import streamlit as st
+import validators
 
 
 def get_component(tag: str) -> List:
@@ -7,3 +12,34 @@ def get_component(tag: str) -> List:
         component = json.load(file)
 
     return component[tag]
+
+
+def add_logo(logo_url: str, height: int = 120):
+    """Add a logo (from logo_url) on the top of the navigation page of a multipage app.
+    Taken from https://discuss.streamlit.io/t/put-logo-and-title-above-on-top-of-page-navigation-in-sidebar-of-multipage-app/28213/6
+
+    The url can either be a url to the image, or a local path to the image.
+
+    Args:
+        logo_url (str): URL/local path of the logo
+    """
+
+    if validators.url(logo_url) is True:
+        logo = f"url({logo_url})"
+    else:
+        logo = f"url(data:image/png;base64,{base64.b64encode(Path(logo_url).read_bytes()).decode()})"
+
+    st.markdown(
+        f"""
+        <style>
+            [data-testid="stSidebarNav"] {{
+                background-image: {logo};
+                background-repeat: no-repeat;
+                background-size: 250px;
+                padding-top: {height - 40}px;
+                background-position: 20px 20px;
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
