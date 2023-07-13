ecd7e0fa66d1dbd8cb95b17e2f7076e61c278a7d
Jiwonii97 <wldnjs3633@naver.com>
2023-07-13 13:17:35 +0900

    feat) 각 파일에 대한 경로 추가

--- streamlit/constraints/PATH.py
+++ streamlit/constraints/PATH.py
@@ -1,6 +1,11 @@
 import os
 
 BASE_PATH = "streamlit" if os.getcwd().split('\\')[-1] != "streamlit" else ""
-SIDEBAR_IMAGE_PATH = os.path.join(BASE_PATH, "assets", "sidebar_logo.png")
+DEMO = os.path.join(BASE_PATH, "demo")
+
+SIDEBAR_IMAGE_PATH = os.path.join(
+    BASE_PATH, "assets", "sidebar_img.png")
 DATA_PATH = os.path.join(BASE_PATH, "assets", "category_ver1.0.0.json")
 IMAGE_ICON_PATH = os.path.join(BASE_PATH, "assets", "music_icon.png")
+ICON_PATH = os.path.join(BASE_PATH, "assets", 'page_icon.png')
+MAIN_IMAGE = os.path.join(BASE_PATH, "assets", 'main_image.png')
