5ec81bcfa5217b93603c799bccfb661ff55388c0
김효연_T5072 <39620398+Broco98@users.noreply.github.com>
2023-07-13 13:32:50 +0900

    style) 변수명, lines 값 변경

--- streamlit/pages/01_choice_category.py
+++ streamlit/pages/01_choice_category.py
@@ -38,22 +38,22 @@ class CategoryChoiceContent():
         """, unsafe_allow_html=True)
 
         # 첫번째 라인
-        col0, col1, col2 = st.columns([2, 13, 3])
-        with col0:        # 아이콘 부분
+        col_0, col_1, col_2 = st.columns([2, 13, 3])
+        with col_0:     # 아이콘 부분
             icon = Image.open(PATH.IMAGE_ICON_PATH).resize((60, 60))
             st.image(icon)
-        with col1:        # 음악 재생 부분
+        with col_1:     # 음악 재생 부분
             st.audio(self.music_file, format='audio/ogg')
-        with col2:
+        with col_2:
             music_caption = '_'.join(self.caption)
             st.download_button(
-                label=":blue[DOWNLOAD]",        # 버튼 라벨 텍스트
+                label=":blue[DOWNLOAD]",    # 버튼 라벨 텍스트
                 key=f"button{str(button_num)}",
                 data=self.music_file,
                 file_name=f"{music_caption}_music.wav"
             )
             button_num += 1     # 버튼은 key값을 지정해야 하기때문에 임의로 Key를 지정
-        space(lines=2)      # 컨텐츠 구분을 짓기 위한 개행 처리
+        space(lines=1)      # 컨텐츠 구분을 짓기 위한 개행 처리
 
 
 # 카테고리 선택 페이지
