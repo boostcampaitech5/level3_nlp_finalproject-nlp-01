bd971f93530193b392c26a5362fb15979aa827dd
김효연_T5072 <39620398+Broco98@users.noreply.github.com>
2023-07-24 17:25:44 +0900

    feat) 422 error case 추가

--- streamlit/utils/config.py
+++ streamlit/utils/config.py
@@ -62,5 +62,7 @@ def set_page():
 def print_error(error_code):
     if error_code == status.HTTP_500_INTERNAL_SERVER_ERROR:
         return "[서버오류] 잠시 후 다시 실행해 주세요"
+    elif error_code == status.HTTP_422_UNPROCESSABLE_ENTITY:
+        return "입력을 확인해 주세요!"
     else:
         return "ERROR_NUM: "+error_code
