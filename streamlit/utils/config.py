a1fa3dcc8babfd0495c802ec74c324c5556a0053
김효연_T5072 <39620398+Broco98@users.noreply.github.com>
2023-07-24 14:04:13 +0900

    feat) delete_another_session_state 알고리즘 수정 -> res_state도 같이 삭제됩니다.

--- streamlit/utils/config.py
+++ streamlit/utils/config.py
@@ -47,7 +47,7 @@ def delete_another_session_state(current_state: str) -> None:
         remain_state (str): 현제 페이지의 session_state
     """
     for state in st.session_state.to_dict():
-        if 'state' in state and state != current_state:
+        if 'state' in state and current_state not in state:
             del st.session_state[state]
 
 
